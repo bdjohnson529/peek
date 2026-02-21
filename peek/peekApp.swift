3f7333a11f5901c40929df1bec68285dcba6779f
Ben Johnson <bdjohnson529@gmail.com>
2026-02-21 11:39:25 +0000

    Initial Commit

---
+++ peek/peekApp.swift
@@ -0,0 +1,17 @@
+//
+//  peekApp.swift
+//  peek
+//
+//  Created by Ben Johnson on 21/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct peekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
